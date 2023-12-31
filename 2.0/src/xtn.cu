b1cacf8a1ad66d0975fbb54383cfc106a2143668
heartnetkung <heartnetkung@gmail.com>
2023-12-31 16:03:30 +0000

    merge stream 2

--- 2.0/src/xtn.cu
+++ 2.0/src/xtn.cu
@@ -6,53 +6,80 @@
 D2Stream<Int2> *b3 = NULL; /*global variable for callback*/
 
 //=====================================
-// Private Functions
+// Private Memory Functions
 //=====================================
 
-MemoryContext cal_memory_stream1(int distance) {
-	MemoryContext ans;
-	ans.gpuSize = get_gpu_memory();
-
+void cal_bandwidth_stream1(MemoryContext &ctx, int distance) {
+	ctx.gpuSize = get_gpu_memory();
 	int deletionMultiplier = (distance == 1) ? (18 + 1) : (153 + 18 + 1);
-	int multiplier = sizeof(int) + //input
-	                 sizeof(int) + //int *combinationOffsets
-	                 deletionMultiplier * (sizeof(Int3) + sizeof(int)); //Int3* &deletionsOutput int* &indexOutput
-	ans.chunkSize = (ans.gpuSize) / (2 * multiplier);
-	ans.chunkCount = (ans.gpuSize + 2 * multiplier - 1) / (2 * multiplier);
+	int multiplier =
+	    sizeof(int) + //input
+	    sizeof(int) + //int *combinationOffsets
+	    //Int3* &deletionsOutput int* &indexOutput unsigned int *histogramValue;
+	    deletionMultiplier * (sizeof(Int3) + 2 * sizeof(int));
+	ctx.bandwidth1 = (7 * ctx.gpuSize) / (10 * multiplier);
+}
+
+void cal_bandwidth_stream2(MemoryContext &ctx) {
+	ctx.gpuSize = get_gpu_memory();
+	int multiplier =
+	    sizeof(Int3) + sizeof(int) + //input
+	    2 * sizeof(int); // int* &inputOffsets, int* &outputLengths
+	ctx.bandwidth1 = ctx.gpuSize / (20 * multiplier);
+	ctx.bandwidth2 = ctx.bandwidth1 * 13;
+}
+
+void cal_bandwidth_stream3(MemoryContext &ctx) {
+	ctx.gpuSize = get_gpu_memory();
+	int multiplier =
+	    sizeof(Int3) + sizeof(int) + //input
+	    2 * sizeof(int) + // int* &inputOffsets, int* &outputLengths
+	    sizeof(char) + sizeof(Int3) + sizeof(int); //char* flags Int3* keyOut int* valueOut;
+	ctx.bandwidth1 = ctx.gpuSize / (10 * multiplier);
+	ctx.bandwidth2 = ctx.bandwidth1 * 6;
+}
+
+void cal_bandwidth_stream4(MemoryContext &ctx) {
+	ctx.gpuSize = get_gpu_memory();
+	int multiplier =
+	    sizeof(Int2) + //input
+	    sizeof(Int2) + //Int2* uniquePairs
+	    2 * sizeof(char) + //char* uniqueDistances, *flags
+	    sizeof(Int2) + //Int2* &pairOutput
+	    sizeof(char);// char* &distanceOutput
+	ctx.bandwidth1 = (7 * ctx.gpuSize) / (10 * multiplier);
+}
+
+MemoryContext cal_memory_stream1(int seq1Len, int distance) {
+	MemoryContext ans;
+	cal_bandwidth_stream1(ans, distance);
+	ans.chunkSize = (seq1Len < ans.bandwidth1) ? seq1Len : ans.bandwidth1;
 	return ans;
 }
 
 MemoryContext cal_memory_stream2() {
 	MemoryContext ans;
-	ans.gpuSize = get_gpu_memory();
-	// ans.maxThroughput;//TODO
+	cal_bandwidth_stream2(ans);
 	// ans.maxThroughputExponent;//TODO
 	return ans;
 }
 
 MemoryContext cal_memory_stream3() {
 	MemoryContext ans;
-	ans.gpuSize = get_gpu_memory();
-	// ans.maxThroughput;//TODO
+	cal_bandwidth_stream3(ans);
 	return ans;
 }
 
 MemoryContext cal_memory_stream4() {
 	MemoryContext ans;
-	ans.gpuSize = get_gpu_memory();
-
-	// memory usage is from
-	// Int2* uniquePairs; char* uniqueDistances, *flags;
-	int multiplier = sizeof(Int2) + //input
-	                 sizeof(Int2) + //Int2* uniquePairs
-	                 2 * sizeof(char) + //char* uniqueDistances, *flags
-	                 sizeof(Int2) + //Int2* &pairOutput
-	                 sizeof(char);// char* &distanceOutput
-	ans.chunkSize = (ans.gpuSize) / (2 * multiplier);
-	ans.chunkCount = (ans.gpuSize + 2 * multiplier - 1) / (2 * multiplier);
+	cal_bandwidth_stream4(ans);
 	// ans.maxThroughputExponent;//TODO
 	return ans;
 }
+
+//=====================================
+// Other Private Functions
+//=====================================
 
 void cal_lowerbounds(int* &lowerbounds, int &lbLen) {
 	lbLen = 1;
@@ -82,10 +109,10 @@ int* concat_clear_histograms(std::vector<int*> histograms, MemoryContext ctx) {
 
 template <typename T1, typename T2>
 int** set_d2_offsets(std::vector<int*> histograms, D2Stream<T1> s1, D2Stream<T2> s2,
-                     int* buffer, MemoryContext ctx) {
+                     int* buffer, int* &offsetLen, MemoryContext ctx) {
 	int** offsets;
 	int* fullHistograms;
-	int len, offsetLen;
+	int len;
 
 	len = histograms.size();
 	fullHistograms = concat_clear_histograms(histograms, ctx);
@@ -125,6 +152,9 @@ void xtn_perform(XTNArgs args, Int3* seq1, void callback(XTNOutput)) {
 	Chunk<int> b1valueChunk, b2valueChunk;
 	Int3* b1keyOut;
 	int* b1valueOut;
+	Int3** keyStorage;
+	int** valueStorage;
+	int* keyStorageLen, *valueStorageLen;
 	printf("2\n");
 
 	cudaMalloc(&deviceInt, sizeof(int)); gpuerr();
@@ -137,7 +167,6 @@ void xtn_perform(XTNArgs args, Int3* seq1, void callback(XTNOutput)) {
 
 	MemoryContext ctx1 = cal_memory_stream1(distance);
 	int outputLen;
-	printf("%lu %d %d\n", ctx1.gpuSize, ctx1.chunkCount, ctx1.chunkSize);
 
 	b0 = new GPUInputStream<Int3>(seq1Device, seq1Len, ctx1.chunkSize);
 	b1key = new D2Stream<Int3>();
@@ -155,28 +184,33 @@ void xtn_perform(XTNArgs args, Int3* seq1, void callback(XTNOutput)) {
 	printf("5\n");
 
 
-	// //=====================================
-	// // stream 2: group key values
-	// //=====================================
+	//=====================================
+	// stream 2: group key values
+	//=====================================
 
-	// MemoryContext ctx2 = cal_memory_stream2();
+	MemoryContext ctx2 = cal_memory_stream2();
+	int chunkCount;
 
-	// offsets = set_d2_offsets(histograms, b1key, b1value, deviceInt, ctx2);
-	// b2keyOutput = new RAMOutputStream<Int3>();//(input, len, len2);
-	// b2valueOutput = new RAMOutputStream<int>();//(input, len, len2);
+	offsets = set_d2_offsets(histograms, b1key, b1value, deviceInt, chunkCount, ctx2);
+	cudaMalloc(&keyStorage, sizeof(Int3*)*chunkCount);
+	cudaMalloc(&valueStorage, sizeof(int*)*chunkCount);
+	cudaMalloc(&keyStorageLen, sizeof(int)*chunkCount);
+	cudaMalloc(&valueStorageLen, sizeof(int)*chunkCount);
+	b2keyOutput = new RAMOutputStream<Int3>(keyStorage, chunkCount, keyStorageLen);
+	b2valueOutput = new RAMOutputStream<int>(valueStorage, chunkCount, valueStorageLen);
 
-	// while ((b1keyChunk = b1key->read()).not_null()) {
-	// 	b1valueChunk = b1value->read();
-	// 	stream_handler2(b1keyChunk, b1valueChunk, histograms,
-	// 	                distance, seq1Len, deviceInt, ctx2);
-	// 	b2keyOutput->write(b1keyChunk, b1keyChunk.len);
-	// 	b2valueOutput->write(b1valueChunk, b1valueChunk.len);
-	// 	_cudaFree(b1keyOut, b1valueOut); gpuerr();
-	// }
+	while ((b1keyChunk = b1key->read()).not_null()) {
+		b1valueChunk = b1value->read();
+		stream_handler2(b1keyChunk, b1valueChunk, histograms,
+		                distance, seq1Len, deviceInt, ctx2);
+		b2keyOutput->write(b1keyChunk, b1keyChunk.len);
+		b2valueOutput->write(b1valueChunk, b1valueChunk.len);
+		_cudaFree(b1keyOut, b1valueOut); gpuerr();
+	}
 
-	// b1key->deconstruct();
-	// b1value->deconstruct();
-	// _cudaFreeHost2D(offsets);
+	b1key->deconstruct();
+	b1value->deconstruct();
+	_cudaFreeHost2D(offsets);
 
 	// //=====================================
 	// // loop: lower bound
