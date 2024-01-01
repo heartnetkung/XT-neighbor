ebd7a6cafd70e8d0732f50f737ad796537e1ccd4
heartnetkung <heartnetkung@gmail.com>
2024-01-01 19:10:15 +0000

    fix warning

--- 2.0/src/xtn_inner.cu
+++ 2.0/src/xtn_inner.cu
@@ -263,7 +263,7 @@ void stream_handler2(Chunk<Int3> &keyInOut, Chunk<int> &valueInOut, std::vector<
 
 	//histogram loop
 	while ((nChunk = solve_next_bin(valueLengthsHost, start, ctx.bandwidth2, offsetLen)) > 0) {
-		printf("stream_handler2() %d %d %d\n");
+		printf("stream_handler2() %d %lu %d\n",start,ctx.bandwidth2,offsetLen);
 		int chunkLen = gen_smaller_index(valueInOut.ptr, inputOffsetsPtr, valueLengthsPtr, indexes, nChunk);
 		cudaMalloc(&histogram, sizeof(int)*ctx.histogramSize);	gpuerr();
 		cal_histogram(indexes, histogram, ctx.histogramSize , 0, seqLen, chunkLen); gpuerr();
