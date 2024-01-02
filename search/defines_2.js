d276d3819e7034a466be445add88ad243a0964fd
heartnetkung <heartnetkung@gmail.com>
2024-01-02 22:51:02 +0000

    update doc

--- search/defines_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['min3',['MIN3',['../kernel_8cu.html#a4887ceeae88266064841cbbc11ef8b53',1,'kernel.cu']]]
-];
--- search/functions_12.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['xtn_5fperform',['xtn_perform',['../xtn_8cu.html#abc5c34200949cf9a94acbd8e3bd69f15',1,'xtn_perform(XTNArgs args, Int3 *seq1, void callback(XTNOutput)):&#160;xtn.cu'],['../xtn_8h.html#abc5c34200949cf9a94acbd8e3bd69f15',1,'xtn_perform(XTNArgs args, Int3 *seq1, void callback(XTNOutput)):&#160;xtn.cu']]]
-];
