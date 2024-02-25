b71a87dfffdbce413af40ea98ad9ba334a6817cc
heartnetkung <heartnetkung@gmail.com>
2024-02-25 15:23:00 +0000

    improve code quality

--- 2.0/src/kernel.cu
+++ 2.0/src/kernel.cu
@@ -38,14 +38,10 @@ void cal_combination_len(Int3* input, int distance, int* output, int n) {
 
 	int len = len_decode(input[tid]);
 	int newValue = 1 + len;
-	switch (distance < len ? distance : len) {
-	case 4:
-		newValue += len * (len - 1) * (len - 2) * (len - 3) / 24;
-	case 3:
-		newValue += len * (len - 1) * (len - 2) / 6;
-	case 2:
+	if (distance == 2)
 		newValue += len * (len - 1) / 2;
-	}
+	// distance larger than 2 is not supported
+
 	output[tid] = newValue;
 }
 
