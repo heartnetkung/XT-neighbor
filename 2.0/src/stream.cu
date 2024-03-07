62cda99ccf87d623c7e09fa1b679a2f5151b593a
heartnetkung <heartnetkung@gmail.com>
2024-03-07 22:40:14 +0000

    add another dummy code

--- 2.0/src/stream.cu
+++ 2.0/src/stream.cu
@@ -129,6 +129,9 @@ public:
 			T* dataHost = _reading_data.back();
 			printf("%d %d\n", ptr == NULL, dataHost == NULL);
 			printf("551 %'lu %'lu\n", sizeof(T), sizeof(T)*len);
+			cudaError_t code = cudaGetLastError();
+			if (code != cudaSuccess)
+				printf("Cudaaa error at %s\n", cudaGetErrorName(code));
 			cudaMemcpy(ptr, dataHost, sizeof(T)*len , cudaMemcpyHostToDevice); gpuerr();
 			printf("552\n");
 			_reading_data.pop_back();
