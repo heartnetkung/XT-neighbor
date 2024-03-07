4965c840f9e7408d3aca82977258c9b390a948a8
heartnetkung <heartnetkung@gmail.com>
2024-03-07 19:41:30 +0000

    update doc

--- search/functions_12.js
+++ search/functions_12.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['xtn_5fperform',['xtn_perform',['../xtn_8cu.html#abcde9c325121ccd75ab2a76b4c400486',1,'xtn_perform(XTNArgs args, Int3 *seq1, int *seqFreq, int *repSizes, void callback(XTNOutput)):&#160;xtn.cu'],['../xtn_8h.html#abcde9c325121ccd75ab2a76b4c400486',1,'xtn_perform(XTNArgs args, Int3 *seq1, int *seqFreq, int *repSizes, void callback(XTNOutput)):&#160;xtn.cu']]]
+  ['xtn_5fperform',['xtn_perform',['../xtn_8cu.html#ac85cffbb3441d48e911a1c618d71e954',1,'xtn_perform(XTNArgs args, Int3 *seq1, int *seqFreqHost, int *repSizesHost, void callback(XTNOutput)):&#160;xtn.cu'],['../xtn_8h.html#ac85cffbb3441d48e911a1c618d71e954',1,'xtn_perform(XTNArgs args, Int3 *seq1, int *seqFreqHost, int *repSizesHost, void callback(XTNOutput)):&#160;xtn.cu']]]
 ];
