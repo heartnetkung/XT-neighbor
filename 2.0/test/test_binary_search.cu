22f4016af3cd77a2befe1a7a50f11db3cf832cb0
heartnetkung <heartnetkung@gmail.com>
2024-03-07 13:16:40 +0000

    update binary search

--- 2.0/test/test_binary_search.cu
+++ 2.0/test/test_binary_search.cu
@@ -1,33 +1,24 @@
 #include "test_util.cu"
 #include "../src/kernel.cu"
-// #include "../src/xtn_inner.cu"
 
 TEST(BinarySearch, {
 	int data[] = {2, 4, 6};
 	int len = 3, input, expect;
 
 	input = 0, expect = 0;
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
-	// check(binarySearch(input, data, len) == expect);
+	check(binarySearch(input, data, len) == expect);
 	input = 1, expect = 0;
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
-	// check(binarySearch(input, data, len) == expect);
+	check(binarySearch(input, data, len) == expect);
 	input = 2, expect = 1;
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
-	// check(binarySearch(input, data, len) == expect);
+	check(binarySearch(input, data, len) == expect);
 	input = 3, expect = 1;
-	// check(binarySearch(input, data, len) == expect);
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
+	check(binarySearch(input, data, len) == expect);
 	input = 4, expect = 2;
-	// check(binarySearch(input, data, len) == expect);
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
+	check(binarySearch(input, data, len) == expect);
 	input = 5, expect = 2;
-	// check(binarySearch(input, data, len) == expect);
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
+	check(binarySearch(input, data, len) == expect);
 	input = 6, expect = 3;
-	// check(binarySearch(input, data, len) == expect);
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
+	check(binarySearch(input, data, len) == expect);
 	input = 7, expect = 3;
-	// check(binarySearch(input, data, len) == expect);
-	printf("%d %d %d \n", input, binarySearch(input, data, len), expect);
+	check(binarySearch(input, data, len) == expect);
 })
