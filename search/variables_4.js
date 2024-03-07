46598625fee2db26142520abe0fe52e6162943c1
heartnetkung <heartnetkung@gmail.com>
2024-03-07 19:26:43 +0000

    update doc

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['entry',['entry',['../structInt3.html#ab291873e871510b4893a5f3699fa2916',1,'Int3']]],
-  ['extendedbuffer',['extendedBuffer',['../structXTNArgs.html#ac8c561d32ef335ec285b84f3e1c21c39',1,'XTNArgs']]]
+  ['entry',['entry',['../structInt3.html#ab291873e871510b4893a5f3699fa2916',1,'Int3']]]
 ];
