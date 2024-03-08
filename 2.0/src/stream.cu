77d355d1699bcfe5392f6f328acc8426757ed97d
heartnetkung <heartnetkung@gmail.com>
2024-03-08 15:25:23 +0000

    add more printing

--- 2.0/src/stream.cu
+++ 2.0/src/stream.cu
@@ -118,6 +118,8 @@ public:
 		cudaFree(_deviceBuffer); gpuerr();
 		printf("AB\n");
 		cudaMalloc(&_deviceBuffer, sizeof(T)*_maxReadableSize); gpuerr();
+		print_gpu_memory();
+		print_main_memory();
 		T* ptr = _deviceBuffer;
 		while (true) {
 			printf("AC\n");
