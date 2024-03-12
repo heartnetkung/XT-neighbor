d614bf35c17bb666e6b09c3735327f58fc7435ab
heartnetkung <heartnetkung@gmail.com>
2024-03-12 13:59:14 +0000

    fix bug

--- 2.0/src/kernel.cu
+++ 2.0/src/kernel.cu
@@ -17,7 +17,6 @@ const size_t MAX = INT_MAX;
 template <typename T>
 T transfer_last_element(T* deviceArr, int n) {
 	T ans[1];
-	printf("aax %d %lu\n", n, sizeof(T));
 	cudaMemcpy(ans, deviceArr + n - 1, sizeof(T), cudaMemcpyDeviceToHost); gpuerr();
 	cudaDeviceSynchronize(); gpuerr();
 	return ans[0];
