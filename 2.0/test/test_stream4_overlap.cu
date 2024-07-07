aee5818055457274e839961999b147f24b82a775
heartnetkung <heartnetkung@gmail.com>
2024-07-07 20:58:28 +0100

    upgrade stream4 overlap

--- 2.0/test/test_stream4_overlap.cu
+++ 2.0/test/test_stream4_overlap.cu
@@ -5,28 +5,28 @@ TEST(Stream4Overlap, {
 	int seqLen = 4;
 	int infoLen = 5;
 	// original sequence char seqs[seqLen][6] = {"CAAA", "CADA", "CAAA", "CDKD", "CAAK"};
-	char seqs[seqLen][6] = {"CAAA", "CADA", "CDKD", "CAAK"};
+	// char seqs[seqLen][6] = {"CAAA", "CADA", "CDKD", "CAAK"};
+	int totalLen = 20;
+	char allStr[] = "CAAACADACAAACDKDCAAK";
 	SeqInfo info_h[] = {
-		{.frequency = 3, .repertoire = 0}, {.frequency = 5, .repertoire = 1},
-		{.frequency = 4, .repertoire = 0}, {.frequency = 6, .repertoire = 1}, {.frequency = 7, .repertoire = 1}
+		{.frequency = 3, .repertoire = 0, .originalIndex = 0},
+		{.frequency = 5, .repertoire = 1, .originalIndex = 2},
+		{.frequency = 4, .repertoire = 0, .originalIndex = 1},
+		{.frequency = 6, .repertoire = 1, .originalIndex = 3},
+		{.frequency = 7, .repertoire = 1, .originalIndex = 4}
 	};
+	unsigned int[] allStrOffset = {0, 4, 8, 12, 16, 20};
 	int inputOffsets[] = {2, 3, 4, 5};
 	int pairLen = 6;
 	int distance = 1;
 
 	//allocate inputs
-	Int3 * seq_d, *seq_h;
-	Int2 * pairs_d, *pairs_h;
+	Int2 *pairs_h;
 	std::vector<XTNOutput> allOutputs;
-	int* deviceInt, *inputOffsets_d;
-	SeqInfo* info_d;
+	int* deviceInt;
 	MemoryContext ctx;
 	cudaMalloc(&deviceInt, sizeof(int));
-	cudaMalloc(&seq_d, sizeof(Int3)*seqLen);
-	cudaMallocHost(&seq_h, sizeof(Int3)*seqLen);
-	cudaMalloc(&pairs_d, sizeof(Int2)*pairLen);
 	cudaMallocHost(&pairs_h, sizeof(Int2)*pairLen);
-	cudaMalloc(&info_d, sizeof(SeqInfo)*infoLen);
 	ctx.bandwidth2 = 100;
 
 	//make inputs
@@ -36,16 +36,17 @@ TEST(Stream4Overlap, {
 	for (int i = 0; i < seqLen; i++)
 		for (int j = i + 1; j < seqLen; j++)
 			pairs_h[count++] = {.x = i, .y = j};
-	seq_d = host_to_device(seq_h, seqLen);
-	pairs_d = host_to_device(pairs_h, pairLen);
-	info_d = host_to_device(info_h, infoLen);
-	inputOffsets_d = host_to_device(inputOffsets, seqLen);
+	char* allStr_d = host_to_device(allStr, totalLen);
+	Int2* pairs_d = host_to_device(pairs_h, pairLen);
+	SeqInfo* info_d = host_to_device(info_h, infoLen);
+	int* inputOffsets_d = host_to_device(inputOffsets, seqLen);
+	unsigned int* allStrOffset_d = host_to_device(allStrOffset, infoLen + 1);
 
 	// do testing
 	Chunk<Int2> pairInput;
 	pairInput.ptr = pairs_d;
 	pairInput.len = pairLen;
-	stream_handler4_overlap(pairInput, allOutputs, seq_d, info_d, inputOffsets_d,
+	stream_handler4_overlap(pairInput, allOutputs, allStr_d, allStrOffset_d, info_d, inputOffsets_d,
 	                        seqLen, distance, LEVENSHTEIN, deviceInt, ctx);
 	XTNOutput output = allOutputs.back();
 
@@ -57,6 +58,6 @@ TEST(Stream4Overlap, {
 	Int2 expectedIndexPair[] = {{.x = 0, .y = 0}, {.x = 0, .y = 1}, {.x = 1, .y = 1}};
 	check_device_arr(output.indexPairs, expectedIndexPair, output.len);
 
-	size_t expectedFrequency[] = {24, 41, 70};
+	size_t expectedFrequency[] = {24, 41, 70}; // 4*3+4*3, 4*5+3*7, 5*7+5*7
 	check_device_arr(output.pairwiseFrequencies, expectedFrequency, output.len);
 })
